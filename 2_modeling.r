83c1418140121e826e84c893dfbb6a93fb834db2
steuben <32399085+kmsteuben@users.noreply.github.com>
2019-03-28 16:52:26 -0700

    updated modeling code for first full run using xgboost

--- 2_modeling.r
+++ 2_modeling.r
@@ -9,6 +9,7 @@ library("Ckmeans.1d.dp") # for xgb.ggplot.importance
 
 
 summary(xy_train)
+xy_train$surface_string <- xy_train$surface
 xy_train$surface <- as.numeric(as.factor(xy_train$surface)) - 1L # for xgboost, label must be ascending integers starting at 0
 
 # Create Test/Train split in datasets and prepare for input into xgboost
@@ -30,6 +31,12 @@ test_data <- data_variables[validation, ]
 test_label <- data_label[validation, ]
 test_matrix <- xgb.DMatrix(data = test_data, label = test_label)
 
+final_variables <- as.matrix(x_test[, .(orientation_X, orientation_Y, orientation_Z, orientation_W,
+                                        angular_velocity_X, angular_velocity_Y, angular_velocity_Z,
+                                        linear_acceleration_X, linear_acceleration_Y, linear_acceleration_Z)])
+final_matrix <- xgb.DMatrix(data = final_variables) 
+
+
 # Cross-validation to Estimate Error
 number_of_classes <- length(unique(data$surface))
 xgb_params <- list("objective" = "multi:softprob",
@@ -38,6 +45,7 @@ xgb_params <- list("objective" = "multi:softprob",
 nround <- 50
 cv.nfold <- 5
 
+# Use this code to do model selection
 cv_model <- xgb.cv(params = xgb_params,
                    data = train_matrix,
                    nrounds = nround,
@@ -54,7 +62,51 @@ confusionMatrix(factor(OOF_prediction$max_prob),
                 factor(OOF_prediction$label),
                 mode = "everything")
 
+# Once model model selection is complete, use this model to make the final model
+bst_model <- xgb.train(params = xgb_params,
+                       data = train_matrix,
+                       nrounds = nround)
 
+# Predict hold-out test set
+test_pred <- predict(bst_model, newdata = test_matrix)
+test_prediction <- matrix(test_pred, nrow = number_of_classes,
+                          ncol = length(test_pred)/ number_of_classes) %>%
+  t() %>%
+  data.frame() %>%
+  mutate(max_prob = max.col(., "last"))
+
+## Importance plots
+names <- colnames(data[, -1])
+
+importance_matrix = xgb.importance(feature_names = names, model = bst_model)
+head(importance_matrix)
+gp = xgb.ggplot.importance(importance_matrix)
+print(gp)
+
+
+final_pred <- predict(bst_model, newdata = final_matrix)
+final_prediction <- matrix(final_pred, nrow = number_of_classes,
+                          ncol = length(final_pred)/ number_of_classes) %>%
+  t() %>%
+  data.frame() %>%
+  mutate(max_prob = max.col(., "last"))
+
+link <- unique(xy_train[, .(surface, surface_string)])
+link$surface <- link$surface + 1
+
+submission <- data.table(series_id = x_test$series_id, max_prob = final_prediction$max_prob)
+View(submission[, .N, by = 'series_id,max_prob'])
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+submission <- submission[, .(best_guess = getmode(max_prob)), by = 'series_id']
+table(submission$best_guess)
+submission <- merge(submission, link, by.x = "best_guess", by.y = "surface")
+submission$best_guess <- NULL
+names(submission) <- c("series_id", "surface")
+submission <- submission[order(series_id),]
+write.csv(submission, file = "xgboost_submission.csv", row.names = FALSE)
 
 ## Using a SVM
 library(kernlab)
